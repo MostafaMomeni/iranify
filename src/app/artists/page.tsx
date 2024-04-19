3bf4951186b55fc07bc4495ecab99a866ffa5850
MostafaMomeni <mustafamomeni1359@gmail.com>
2024-04-19 16:58:58 +0330

    finish artists page

---
+++ src/app/artists/page.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import style from "./Artists.module.css"
+import SectionHeader from '@/components/template/SectionHeader/SectionHeader'
+import ArtistBox from '@/components/template/ArtistBox/ArtistBox'
+
+export default function page() {
+  return (
+    <div className={style.parent}>
+        <SectionHeader title="خواننده ها"/>
+        <div className={style.artists_parent}>
+            <ArtistBox title="یاس" src="yas.webp" link="/artists/yas"/>
+            <ArtistBox title="یاس" src="yas.webp" link="/artists/yas"/>
+            <ArtistBox title="یاس" src="yas.webp" link="/artists/yas"/>
+            <ArtistBox title="یاس" src="yas.webp" link="/artists/yas"/>
+            <ArtistBox title="یاس" src="yas.webp" link="/artists/yas"/>
+            <ArtistBox title="یاس" src="yas.webp" link="/artists/yas"/>
+            <ArtistBox title="یاس" src="yas.webp" link="/artists/yas"/>
+            <ArtistBox title="یاس" src="yas.webp" link="/artists/yas"/>
+            <ArtistBox title="یاس" src="yas.webp" link="/artists/yas"/>
+            <ArtistBox title="یاس" src="yas.webp" link="/artists/yas"/>
+        </div>
+    </div>
+  )
+}
