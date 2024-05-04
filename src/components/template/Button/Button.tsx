d4d4ea9ee66cf7cd24cd171fbad290a514063de5
MostafaMomeni <mustafamomeni1359@gmail.com>
2024-05-04 17:39:37 +0330

    update button card

--- src/components/template/Button/Button.tsx
+++ src/components/template/Button/Button.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import style from "./Button.module.css";
 
-export default function Button({text}:any) {
+export default function Button({ text }: any) {
   return (
     <>
-      <button className={style.button}>{text}</button>
+      <button className={style.button}>
+        <p>{text}</p>
+      </button>
     </>
   );
 }
