554cce1306057e97b388eab6ff41f0a945d75862
MostafaMomeni <mustafamomeni1359@gmail.com>
2024-05-13 21:36:38 +0330

    start

--- src/components/module/Slider/Slider.tsx
+++ src/components/module/Slider/Slider.tsx
@@ -6,7 +6,7 @@ import "swiper/css";
 import "swiper/css/effect-fade";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import { Autoplay , EffectFade, Navigation, Pagination } from "swiper/modules";
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
 import Image from "next/image";
 
 export default function Slider() {
